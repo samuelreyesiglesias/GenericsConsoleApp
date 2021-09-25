8fbfd2327de344c13e7b73295cab729ade201f1a
samuel <samuelreyesiglesias@gmail.com>
2021-09-24 20:07:10 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GenericsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyData DataToUseInGeneric = new MyData();
+            DataToUseInGeneric.Number1 = 1000;
+            Generic1<MyData> GenericClassImplementationInstance = new Generic1<MyData>(DataToUseInGeneric);
+            Generic1<MyData>.ShowNumber();
+
+        }
+    }
+    interface IGeneric1
+    {
+        int Number1 { get; set; }
+    }
+    class MyData : IGeneric1
+    {
+        public int Number1 { get; set; }
+    }
+    class Generic1<T> where T : IGeneric1
+    {
+        public static T data;
+       public Generic1(T N)
+        {
+            data = N;
+        }
+
+        public static void ShowNumber()
+        {
+            Console.WriteLine($"The number you sent to the class was:"+ data.Number1.ToString());
+        }
+    }
+}
